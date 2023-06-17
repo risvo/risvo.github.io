093cc08f95341985feae2bddca27a89fee04953a
CapMar00 <CapMar00@users.noreply.github.com>
2023-06-17 12:50:21 +0200

    minor changes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+/* HEADER */
+
+const desktop_navbar = document.querySelector(".d-menu");
+const navbar_items = [
+  { text: "Chi siamo", link: "./pages/chi-siamo.html" },
+  { text: "Cosa facciamo", link: "./pages/cosa-facciamo.html" },
+  { text: "Open data", link: "./pages/open-data.html" },
+  { text: "Media", link: "./pages/media.html" },
+  { text: "Partners", link: "./pages/partners.html" },
+];
+
+/* FOOTER */
 const today = new Date();
 const present_year = today.getFullYear();
 document
