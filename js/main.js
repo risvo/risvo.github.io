1193743f3a41fad6dcc761c0fde7be9e40c849db
CapMar00 <CapMar00@users.noreply.github.com>
2023-06-17 18:05:35 +0200

    added hamburger menu

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
-/* HEADER */
-
-const desktop_navbar = document.querySelector(".d-menu");
+const mobileTabletWidth = window.matchMedia("(max-width: 820px)");
 const navbar_items = [
   { text: "Chi siamo", link: "./pages/chi-siamo.html" },
   { text: "Cosa facciamo", link: "./pages/cosa-facciamo.html" },
@@ -9,11 +7,82 @@ const navbar_items = [
   { text: "Partners", link: "./pages/partners.html" },
 ];
 
-/* FOOTER */
-const today = new Date();
-const present_year = today.getFullYear();
-document
-  .querySelector(".f-credits")
-  .querySelector(
-    "span"
-  ).innerHTML = `&copy;${present_year}, tutti i diritti riservati`;
+const createLink = (item) => {
+  let link = document.createElement("a");
+  link.href = item.link;
+  link.text = item.text;
+  return link;
+};
+
+const renderNavItem = (item, index, viewport = "desktop") => {
+  let li = document.createElement("li");
+  if ((viewport == "desktop") & (index === 0)) {
+    li.className = "pr-1 ptb-1";
+  } else if (index === navbar_items.length - 1) {
+    li.className = "pl-1 ptb-1";
+  } else {
+    li.className = "plr-1 ptb-1";
+  }
+
+  li.appendChild(createLink(item));
+  return li;
+};
+
+const scrollToElem = (selector, yOffset = 0) => {
+  const el = document.querySelector(selector);
+  const y = el.getBoundingClientRect().top + window.pageYOffset + yOffset;
+  window.scrollTo({ top: y, behavior: "smooth" });
+};
+
+window.addEventListener("DOMContentLoaded", () => {
+  /* HEADER */
+  const desktop_navbar = document.querySelector(".d-menu").querySelector("ul");
+  const mobile_navbar = document.querySelector(".m-menu").querySelector("ul");
+
+  if (mobileTabletWidth.matches) {
+    navbar_items.forEach((item, index) => {
+      mobile_navbar.appendChild(renderNavItem(item, index, "mobile"));
+    });
+  } else {
+    navbar_items.forEach((item, index) => {
+      desktop_navbar.appendChild(renderNavItem(item, index));
+    });
+  }
+
+  /* FOOTER */
+  const today = new Date();
+  const present_year = today.getFullYear();
+  const footer = document.querySelector("footer");
+  footer.innerHTML = `
+                      <section class="footer pt-7">
+                      <div class="footer-col f-credits text-center pr-1 m-plr-0">
+                          <h5>Risvo</h5>
+                          &copy;${present_year}, tutti i diritti riservati
+                      </div>
+                      <div class="footer-col text-center plr-1 m-plr-0">
+                          <h5>Contatti</h5>
+                          <a href="mailto:federico.monaco@unipr.it">federico.monaco@unipr.it</a>
+                          SIM.LAB Laboratorio didattico per la simulazione in Medicina - Dipartimento di Medicina e Chirurgia -
+                          Università di Parma
+                      </div>
+                      <div class="footer-col text-left plr-1 m-plr-0">
+                          <h5>Policy</h5>
+                          <ul>
+                              <li><a href="./pages/privacy-policy.html">Privacy</a></li>
+                              <li><a href="./pages/cookies-policy.html">Cookie</a></li>
+                          </ul>
+                      </div>
+                      <div class="footer-col text-left pl-1 m-plr-0">
+                          <h5>Social</h5>
+                          <ul class="f-social">
+                              <li><a href="https://risvo.github.io/" target="_blank" rel="noopener noreferrer">Github</a></li>
+                              <li><a href="https://www.youtube.com/watch?v=1DvkMKSjWRc" target="_blank"
+                                      rel="noopener noreferrer">Youtube</a></li>
+                              <li><a href="http://" target="_blank" rel="noopener noreferrer">Instagram</a></li>
+                              <li><a href="https://discord.com/channels/1110201669154787328/1110201671423901826" target="_blank"
+                                      rel="noopener noreferrer">Discord</a></li>
+                          </ul>
+                      </div>
+                    </section>  
+                    `;
+});
